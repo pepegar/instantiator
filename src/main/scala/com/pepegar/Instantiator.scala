58a72df275fe32b89eae5983b2d8c2ecfa8f85da
Pepe García <joseluisgarcia@packlink.es>
2015-02-20 12:26:12 +0100

    feat(type tree): create the type tree
    Now, given a type, we are creating the type tree for all the properties
    of the given type.

--- src/main/scala/com/pepegar/Instantiator.scala
+++ src/main/scala/com/pepegar/Instantiator.scala
@@ -2,16 +2,42 @@ package com.pepegar
 
 import scala.reflect.runtime.universe._
 
-abstract class TypeTree
-abstract class Branch(a: TypeTree*) extends TypeTree
-abstract class Leaf[T](value: T) extends TypeTree
+sealed abstract class Tree
+case class Branch[T](owner: T, properties: List[Tree]) extends Tree
+case class Leaf[T](value: T) extends Tree
 
+/** Instantiator object is the main object and entry point to the library.
+ *
+ * it exposes a single function, ```createInstance``` which is a generic method. That createInstance
+ * method takes a type as parameter and returns a valid instance of it.
+ *
+ * @author pepegar
+ */
 object Instantiator {
+
+  /** Nothing to see here yet
+   *
+   * @author pepegar
+   */
   def createInstance[T](implicit tag: TypeTag[T]) = {
     val t = typeOf[T]
-    val params = t.members.filter(!_.isMethod).map(s => (s.name, s.typeSignature))
-    params
+    generateTypesTree(t)
   }
 
-  //def generateTree(l: List[(TermName, )]) = 
+  /** This method generates the type tree of properties of the given type
+   *
+   * The purpose of it is creating a Tree that will be parsed later in the moment of generating the
+   * instance of the class.
+   *
+   * @author pepegar
+   * */
+  def generateTypesTree(tpe: Type): Tree = {
+    val symbol = tpe.typeSymbol
+    val classProperties = tpe.members.filter(!_.isMethod)
+
+    classProperties.isEmpty match {
+      case true => Leaf(symbol.name)
+      case false => Branch(symbol.name, classProperties.map(s => generateTypesTree(s.typeSignature)).toList)
+    }
+  }
 }
--- src/test/scala/com/pepegar/InstantiatorSpec.scala
+++ src/test/scala/com/pepegar/InstantiatorSpec.scala
@@ -1,18 +1,28 @@
 package com.pepegar
 
 import org.scalatest._
-
-case class A(b: B, c: C)
-case class B(d: D)
-case class C(d: D)
-case class D()
+import scala.reflect.runtime.universe._
 
 class InstantiatorSpec extends FunSpec {
-  val ret = Instantiator.createInstance[A]
+  case class A(b: B, c: C)
+  case class B(d: D)
+  case class C(d: D)
+  case class D(left: Int, right: String)
 
-  println()
-  println(ret)
-  println()
+  describe("Instantiator") {
+    describe("generateTypesTree") {
+      it("should create a valid tree given a type instance.") {
+        val t = typeOf[A]
+        val typesTree = Instantiator.generateTypesTree(t)
 
-  assert(false)
+        typesTree match {
+          case Branch(name, props) => {
+            assert(props.length === 2) // A has 2 properties
+            assert(name.toString === "A")
+          }
+          case _ =>
+        }
+      }
+    }
+  }
 }
