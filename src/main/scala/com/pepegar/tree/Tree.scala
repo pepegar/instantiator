27ff5ed8caa863187deaf763c211a257daef1daf
Pepe García <joseluisgarcia@packlink.es>
2015-02-25 12:24:59 +0100

    feat(map): implemented map for Branch

--- src/main/scala/com/pepegar/tree/Tree.scala
+++ src/main/scala/com/pepegar/tree/Tree.scala
@@ -1,5 +1,9 @@
 package com.pepegar.tree
 
 abstract class Tree
-case class Branch[T, U](owner: T, properties: List[U]) extends Tree
+case class Branch[T, U](owner: T, properties: List[U]) extends Tree {
+  def map[V](f: U => V) = {
+    Branch(owner,properties.map(f))
+  }
+}
 case class Leaf[T](value: T) extends Tree
--- src/test/scala/com/pepegar/tree/TreeSpec.scala
+++ src/test/scala/com/pepegar/tree/TreeSpec.scala
@@ -1,1 +1,19 @@
 package com.pepegar.tree
+
+import org.scalatest._
+
+class TreeSpec extends FunSpec {
+  describe("Branch") {
+    describe("map") {
+      it("converts a Branch[Tree] to Branch[T]") {
+        def intToString(i: Int): String = i.toString
+        val integerBranch = Branch(4, List(1,2,3,4))
+        val stringBranch = integerBranch.map[String](intToString)
+
+        stringBranch.properties.foreach { elem =>
+          assert(elem.isInstanceOf[String])
+        }
+      }
+    }
+  }
+}
