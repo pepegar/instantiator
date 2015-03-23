b5b8824c8511274e3d24138f1d82a906f7049a25
Pepe García <jl.garhdez@gmail.com>
2015-03-23 15:17:00 +0100

    test(instantiator): add tests for simple classes

--- src/test/scala/com/pepegar/InstantiatorSpec.scala
+++ src/test/scala/com/pepegar/InstantiatorSpec.scala
@@ -2,24 +2,42 @@ package com.pepegar
 
 import org.scalatest._
 
+case class TwoInts(a: Int, b: Int)
+case class TwoFloats(a: Float, b: Float)
+case class TwoStrings(a: String, b: String)
+case class StringInt(a: String, b: Int)
+
 class InstantiatorSpec extends FunSpec with Matchers {
-  it("instantiates primitives correctly") {
-    val maybeInt = Instantiator.createInstance[Int]
-    maybeInt shouldBe a [java.lang.Integer]
+  describe("createInstance") {
+    it("instantiates primitives correctly") {
+      val maybeInt = Instantiator.createInstance[Int]
+      maybeInt shouldBe a [java.lang.Integer]
 
-    val maybeString = Instantiator.createInstance[String]
-    maybeString shouldBe a [java.lang.String]
+      val maybeString = Instantiator.createInstance[String]
+      maybeString shouldBe a [java.lang.String]
 
-    val maybeFloat = Instantiator.createInstance[Float]
-    maybeFloat shouldBe a [java.lang.Float]
+      val maybeFloat = Instantiator.createInstance[Float]
+      maybeFloat shouldBe a [java.lang.Float]
 
-    val maybeByte = Instantiator.createInstance[Byte]
-    maybeByte shouldBe a [java.lang.Byte]
+      val maybeByte = Instantiator.createInstance[Byte]
+      maybeByte shouldBe a [java.lang.Byte]
 
-    val maybeBoolean = Instantiator.createInstance[Boolean]
-    maybeBoolean shouldBe a [java.lang.Boolean]
+      val maybeBoolean = Instantiator.createInstance[Boolean]
+      maybeBoolean shouldBe a [java.lang.Boolean]
 
-    val maybeShort = Instantiator.createInstance[Short]
-    maybeShort shouldBe a [java.lang.Short]
+      val maybeShort = Instantiator.createInstance[Short]
+      maybeShort shouldBe a [java.lang.Short]
+    }
+
+    it("instantiates simple classes correctly") {
+      val maybeTwoInts = Instantiator.createInstance[TwoInts]
+      maybeTwoInts shouldBe a [TwoInts]
+
+      val maybeTwoFloats = Instantiator.createInstance[TwoFloats]
+      maybeTwoFloats shouldBe a [TwoFloats]
+
+      val maybeTwoStrings = Instantiator.createInstance[TwoStrings]
+      maybeTwoStrings shouldBe a [TwoStrings]
+    }
   }
 }
